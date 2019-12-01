b552247b823489f4414a5ac6d266429bdd95f195
MohamedElmdary <engm5081@gmail.com>
2019-12-01 07:32:44 +0200

    binary-search-tree

--- src/data-structure/binary-trees/binary-search-tree.ts
+++ src/data-structure/binary-trees/binary-search-tree.ts
@@ -1,3 +1,5 @@
+import { Queue } from '../index';
+
 class BSTNode<T> {
     constructor(
         public value: T,
@@ -50,6 +52,82 @@ class BinarySearchTree<T> {
         }
         return false;
     }
+
+    public bfs(fn: (value: T) => void): void {
+        if (!this.root) {
+            return;
+        }
+        const queue = new Queue<BSTNode<T>>();
+        queue.enqueue(this.root);
+        while (queue.length) {
+            const node = queue.dequeue();
+            fn(node.value);
+            if (node.left) queue.enqueue(node.left);
+            if (node.right) queue.enqueue(node.right);
+        }
+    }
+
+    public dfsPreOrder(fn: (value: T) => void): void {
+        if (!this.root) {
+            return;
+        }
+        function traverse(node: BSTNode<T>): void {
+            fn(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+    }
+
+    public dfsPostOrder(fn: (value: T) => void): void {
+        if (!this.root) {
+            return;
+        }
+        function traverse(node: BSTNode<T>): void {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            fn(node.value);
+        }
+        traverse(this.root);
+    }
+
+    public dfsInOrder(fn: (value: T) => void): void {
+        if (!this.root) {
+            return;
+        }
+        function traverse(node: BSTNode<T>): void {
+            if (node.left) traverse(node.left);
+            fn(node.value);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+    }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(10)
+    .insert(6)
+    .insert(15)
+    .insert(3)
+    .insert(8)
+    .insert(20);
+
+tree.dfsPreOrder(v => {
+    v;
+});
+
+tree.dfsPostOrder(v => {
+    v;
+});
+
+tree.dfsInOrder(v => {
+    v;
+});
+
+tree.bfs(v => {
+    console.log(v);
+});
+
+console.log(tree);
 
 export { BSTNode, BinarySearchTree };
